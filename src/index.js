47ac3c3d6f43944f939358859c3fd6c93c38ea9f
Sten Dembitsky <52003454+Dembitsky@users.noreply.github.com>
2022-07-31 10:20:49 +0300

    one config

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.html';
+import './index.scss';
+import code from './img/logo-12.png';
+import {mult,sum} from './modules/calc';
+
+
+
+console.log(mult(4,5));
+console.log(sum(5, 6));
